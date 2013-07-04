ad0be7cf96168da7b98642e93035495c8b6321a7
Spoonunit <spoonbase@googlemail.com>
2013-07-04 08:24:05 +0000

    get content from file

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var express = require('express'),
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var hello = fs.readFileSync("index.html");
+    var hello = fs.readFileSync("index.html", {encoding: "utf-8"});
     response.send(hello);
 });
 
