a62a5c5999af01c6a7e23d044808fc588bd1f49e
Spoonunit <spoonbase@googlemail.com>
2013-07-05 18:39:11 +0100

    grader.js solution for url

--- grader.js
+++ grader.js
@@ -27,7 +27,6 @@ var cheerio = require('cheerio');
 var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URL_DEFAULT = "http://www.google.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -37,11 +36,6 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
-
-var getURL = function (infile) {
-    var instr = infile.toString();
-    return instr;
-}
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -64,15 +58,14 @@ var checkHtmlFile = function(htmlfile, checksfile) {
 
 var checkHtmlURL = function(url, checksfile) {
     restler.get(url).on('complete', function (result, response) {
-	$ = cheerio.load(result);
-	console.log(response);
+	$ = cheerio.load(response.rawEncoded);
 	var checks = loadChecks(checksfile).sort();
 	var out = {};
 	for(var ii in checks) {
             var present = $(checks[ii]).length > 0;
             out[checks[ii]] = present;
 	}
-	return out;
+	console.log(JSON.stringify(out, null, 4));
     });
 }
 
@@ -80,17 +73,14 @@ if(require.main == module) {
     program
         .option('-c, --checks ', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
         .option('-f, --file ', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
-        .option('-u, --url ', 'URL to index.html', getURL, URL_DEFAULT)
+        .option('-u, --url [value]', 'URL to index.html')
         .parse(process.argv);
     if (program.file) {
 	var checkJson = checkHtmlFile(program.file, program.checks);
 	var outJson = JSON.stringify(checkJson, null, 4);
 	console.log(outJson);
     } else if (program.url) {
-	console.log("url:"+program.url);
-	var checkJson = checkHtmlURL(program.url, program.checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);
+	checkHtmlURL(program.url, program.checks);
     }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
